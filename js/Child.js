e5336049ef00697f2d79a5beb99456972a7db2e9
P.P <s.pp0415.jp@gmail.com>
2025-05-03 17:16:22 +0900

    test

--- js/Child.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    // 親ウィンドウの存在チェック
-    console.log("open")
-    if (!window.opener || window.opener.closed) {
-        window.alert('親ウィンドウがありません。');
-        return false;
-    }
-}
